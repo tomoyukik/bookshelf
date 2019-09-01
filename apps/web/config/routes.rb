9e53ed8a5e14501e93f90fc9c4bfeb26b2a884cb
tomoyukik <kobori.tomoyuki@gmail.com>
2019-09-01 12:35:36 +0900

    update: updated routings
    	modified:   apps/web/config/routes.rb
    	modified:   apps/web/controllers/books/create.rb
    	modified:   apps/web/templates/books/new.html.erb

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -3,6 +3,4 @@
 # See: https://guides.hanamirb.org/routing/overview
 
 root to: 'home#index'
-get '/books', to: 'books#index'
-get '/books/new', to: 'books#new'
-post '/books', to: 'books#create'
+resources :books, only: %i[index new create]
